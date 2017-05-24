8739c3bd2c0369a8a59038ce3483f646a7b532ec
exrienz <exrienz@gmail.com>
2017-05-24 11:18:27 +0800

    Update s1mpl3.sh

--- s1mpl3.sh
+++ s1mpl3.sh
@@ -222,6 +222,7 @@ runSelfUpdate() {
   if ! wget --quiet --output-document="$0.tmp" $UPDATE_BASE > $SELF ; then
     echo "Failed: Error while trying to wget new version!"
     echo "File requested: $UPDATE_BASE/$SELF"
+	sleep 5
     exit 1
   fi
   echo "Done."
