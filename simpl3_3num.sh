4f04aab441dd198df0b851a9aed3ac1ebac4d168
exrienz <exrienz@gmail.com>
2017-09-08 10:57:01 +0800

    Update simpl3_3num.sh

--- simpl3_3num.sh
+++ simpl3_3num.sh
@@ -11,7 +11,7 @@
 
 
 #System
-declare -r app_version='BETA 1.8'
+declare -r app_version='BETA 1.9'
 
 
 #Auto Update Script
@@ -713,6 +713,7 @@ function active_web_crawler_module {
 	mkdir -p $report_path$hosts 2> /dev/null
 	# echo ""
 	# skipfish -d $depth $the_cookies -o $report_path$hosts/$output $protocols://$hosts;
+	echo -e "$OKRED	[✔-OK!]::[Progress]: Crawling in progress..Please Wait! $RESET"
 	./$application_path$domain_analyzer_folder/crawler.py -u $hosts -s -m 100 | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | sed "s/\x0f//g" |& tee -a  $report_path$hosts/$output.txt;
 	x-www-browser $report_path$hosts/$output.txt 2> /dev/null &
 	# x-www-browser $report_path$hosts/$output/index.html 2> /dev/null &
